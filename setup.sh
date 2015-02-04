9fa189fad13da2860e333c6d008f7e9fc4967c6d
Kevin Corbin <kecorbin@cisco.com>
2015-02-04 11:59:10 -0600

    cleanup

--- setup.sh
+++ setup.sh
@@ -5,14 +5,15 @@ APIC_USERNAME=admin
 APIC_PASSWORD=ins3965!
 
 #build the required nodes
-#sudo docker build -t db db && sudo docker build app && sudo docker build web
+sudo docker build -t db db && sudo docker build -t app app && sudo docker build -t web web && sudo docker build -t viz viz
 
-#launch the nodes
-#sudo docker run -d --name db db
-sudo docker run -p 5000:5000 --link db:db --name web web
+#launch the db node
+sudo docker run -d --name db db
+#launch the app node
+sudo dockerer run -d --name app --link db:db app /opt/acitoolkit/applications/endpointtracker/aci-endpoint-tracker.py -u http://$APIC_IP -l $APIC_USERNAME -p $APIC_PASSWORD -i db -a root -s ''
+# launch presentation nodes
 sudo docker run -p 5001:5001 --link db:db --name viz viz
-sudo docker run -d --name app --link db:db app /opt/acitoolkit/applications/endpointtracker/aci-endpoint-tracker.py -u http://$APIC_IP -l $APIC_USERNAME -p $APIC_PASSWORD -i db -a root -s ''
-
+sudo docker run -p 500:5000 --link db:db --name web web
 
 
 
